f9a5d64077209e921d94491cb7b87ded46e734dc
于军 <627265811@qq.com>
2020-02-13 22:25:14 +0800

    新增基本目录结构

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,20 +1,37 @@
-//app.js
+const { version, stage } = require('./config');
+require('./utils/libs.js');
+//初始化云开发
+wx.cloud.init({
+  env: stage,
+  traceUser: true,
+})
 App({
   onLaunch: function () {
-    
-    if (!wx.cloud) {
-      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
-    } else {
-      wx.cloud.init({
-        // env 参数说明：
-        //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
-        //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
-        //   如不填则使用默认环境（第一个创建的环境）
-        // env: 'my-env-id',
-        traceUser: true,
+
+    /**
+     * 更新版本
+     */
+    if (wx.canIUse('getUpdateManager')) {
+      const updateManager = wx.getUpdateManager();
+      updateManager.onCheckForUpdate(function (res) {
+        // 请求完新版本信息的回调
+        console.log("是否更新版本:",res.hasUpdate)
+      })
+      updateManager.onUpdateReady(function () {
+        wx.showModal({
+          content: '发现新版本，请重启当前小程序~',
+          confirmColor: '#ffbb12',
+          confirmText: '确定重启',
+          showCancel: false,
+          success: function (res) {
+            if (res.confirm) {
+              // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+              updateManager.applyUpdate()
+            }
+          }
+        })
       })
     }
-
     this.globalData = {}
   }
 })
